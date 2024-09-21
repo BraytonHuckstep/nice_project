64bff4e2e6f62782db86d4a6b28aa30e31158f59
BraytonHuckstep <164892606+BraytonHuckstep@users.noreply.github.com>
2024-09-21 14:32:20 -0400

    Create 404.js
    create

---
+++ 404.js
@@ -0,0 +1,12 @@
+<%- include('./partials/head.ejs') %>
+<body>
+    <%- include('./partials/nav.ejs') %>
+
+    <div class="about content">
+        <h1>About</h1>
+        <p>This is a simple blog application using Node.js and EJS as the view engine.</p>
+    </div>
+
+    <%- include('./partials/footer.ejs') %>
+</body>
+</html>
